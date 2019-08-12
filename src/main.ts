fcc622dd0ac09eeb759410031f553bf1ca558a17
liukai <3001397597@qq.com>
2019-08-12 11:52:00 +0800

    提交模板

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,21 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import './plugins/element.js';
+
+// 引入icons
+import './assets/iconfont/iconfont.css';
+// 引入公用的mixins
+const Mixin = require('./mixins')
+Vue.mixin(Mixin)
+
+// 引入公用过滤器
+const filters = require('./filters')
+
+// 设置全局过滤器
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
 
 Vue.config.productionTip = false;
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,21 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+@Component
+export default class About extends Vue {
+  @Prop() private msg!: string;
+
+  @Prop({ default: "default value" }) private propB!: string;
+
+  @Watch("propB", { immediate: true, deep: true })
+  msgChange(val: "propB") {
+    console.log(val);
+  }
+}
+</script>
+
+
+
